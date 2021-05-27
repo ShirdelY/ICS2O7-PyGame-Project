783ddda2687e3167ad66d666733381ccc99df332
ShirdelY <>
2021-05-27 01:14:38 +0000

    main.py

--- main.py
+++ main.py
@@ -28,22 +28,31 @@ gameDisplay = pygame.display.set_mode(size)
 
 #initialize the title and icon
 pygame.display.set_caption("Polyvasion")
-icon = pygame.image.load("angrycircle.png")
+icon = pygame.image.load("Assets/angrycircle.png")
 pygame.display.set_icon(icon)
 
 #initialize the backgrounds
-titlescreen_background = pygame.
+titlescreen_background = pygame.image.load("Assets/blue_polygons_background.png")
+titlescreen_background = pygame.transform.scale(titlescreen_background, size)
 
-
-
-
-
-
-
-
-
-
-
-#fonts
+#initialize the fonts
 bigfont = pygame.font.SysFont("arial",50)
 smallfont = pygame.font.SysFont("arial",15)
+
+#declare variables
+quiz_score = 0
+
+#title loop
+def titlescreen():
+  screen.blit(titlescreen_background)
+  TitleScreen = True
+  x = 0
+  y = 0    
+  while TitleScreen:
+      for event in pygame.event.get():
+          print(event)
+          if event.type == pygame.QUIT:
+              pygame.quit()
+              exit()
+
+  
